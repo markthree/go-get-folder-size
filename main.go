da58327227c95006a2976d7a352b1fde7a7aaeba
markthree <1801982702@qq.com>
2023-01-27 16:27:46 +0800

    feat: add cli sync

--- main.go
+++ main.go
@@ -14,11 +14,21 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	size, err := getFolderSize.Parallel(root)
-	if err != nil {
-		fmt.Println(err)
-		return
+
+	if len(os.Args) > 1 && os.Args[1] == "-s" {
+		size, err := getFolderSize.Sync(root)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		print(bytefmt.ByteSize(uint64(size)))
+	} else {
+		size, err := getFolderSize.Parallel(root)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		print(bytefmt.ByteSize(uint64(size)))
 	}
 
-	print(bytefmt.ByteSize(uint64(size)))
 }
