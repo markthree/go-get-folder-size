35af69aaf5e3f6de29b3ef506914b0ad423d60f3
markthree <1801982702@qq.com>
2023-01-28 17:12:51 +0800

    feat: add wasm

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ import '../wasm/polyfill.js'
 import '../wasm/wasm_exec.js'
 import init from '../wasm/main.wasm?init'
 
-const go = new global.Go()
-
-init(go.importObject).then(instance => {
-	return go.run(instance)
-})
+// Not recommended. It may be slower than the native node
+export async function getFolderSizeWasm(
+	path: string,
+	pretty = false
+) {
+	const go = new global.Go()
+	go.env = { path, pretty }
+	const instance = await init(go.importObject)
+	await go.run(instance)
+	if (global.$folderSizeError) {
+		throw new Error(global.$folderSizeError)
+	}
+	const size = global.$folderSize
+	global.$folderSize = null
+	return size as number | string
+}
--- wasm/main.go
+++ wasm/main.go
@@ -1,6 +1,26 @@
 // js-wasm
 package main
 
+import (
+	"os"
+	"syscall/js"
+
+	"code.cloudfoundry.org/bytefmt"
+	getFolderSize "github.com/markthree/go-get-folder-size/src"
+)
+
 func main() {
-	print("hello")
+	value, err := getFolderSize.Parallel(os.Getenv("path"))
+
+	if err != nil {
+		js.Global().Set("$folderSizeError", err)
+		return
+	}
+
+	if os.Getenv("pretty") == "true" {
+		js.Global().Set("$folderSize", bytefmt.ByteSize(uint64(value)))
+		return
+	}
+
+	js.Global().Set("$folderSize", value)
 }
