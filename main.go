aed11073ba7b020efa57d7a3d3de96d3829e7bea
markthree <1801982702@qq.com>
2023-01-28 18:32:37 +0800

    build: add goreleaser

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import (
 	getFolderSize "github.com/markthree/go-get-folder-size/src"
 )
 
+func sliceIncludes(s []string, o string) bool {
+	for _, v := range s {
+		if v == o {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	root, err := os.Getwd()
 	if err != nil {
@@ -16,19 +25,27 @@ func main() {
 		return
 	}
 
-	if len(os.Args) > 1 && os.Args[1] == "-s" {
+	var _size int64
+
+	if sliceIncludes(os.Args, "-s") {
 		size, err := getFolderSize.Sync(root)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		print(bytefmt.ByteSize(uint64(size)))
+		_size = size
 	} else {
 		size, err := getFolderSize.Parallel(root)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		print(bytefmt.ByteSize(uint64(size)))
+		_size = size
+	}
+
+	if sliceIncludes(os.Args, "-p") {
+		print(bytefmt.ByteSize(uint64(_size)))
+	} else {
+		print(uint64(_size))
 	}
 }
