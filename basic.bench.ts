d20c54a4aebe91ee467e20e9378465600c0b8b7d
markthree <1801982702@qq.com>
2023-01-29 17:55:16 +0800

    chore: add bench

---
+++ basic.bench.ts
@@ -0,0 +1,21 @@
+import { bench, describe } from 'vitest'
+import {
+	getFolderSize,
+	getFolderSizeBin,
+	getFolderSizeWasm
+} from './npm'
+
+describe('basic', () => {
+	const base = `./`
+	bench('getFolderSize', async () => {
+		await getFolderSize(base)
+	})
+
+	bench('getFolderSizeBin', async () => {
+		await getFolderSizeBin(base)
+	})
+
+	bench('getFolderSizeWasm', async () => {
+		await getFolderSizeWasm(base)
+	})
+})
