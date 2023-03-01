8c66afb54b737d03f4557753a61d6786f4a3a76d
markthree <1801982702@qq.com>
2023-03-01 13:16:36 +0800

    fix(wasm): polyfill require

--- wasm/polyfill.js
+++ wasm/polyfill.js
@@ -3,25 +3,27 @@
 // license that can be found in the LICENSE file.
 // The polyfill is changed from wasm_exec_node.js
 
-'use strict'
+"use strict";
 
-globalThis.require = require
-globalThis.fs = require('fs')
-globalThis.TextEncoder = require('util').TextEncoder
-globalThis.TextDecoder = require('util').TextDecoder
+import { createRequire } from "node:module";
+
+globalThis.require ??= require ?? createRequire(import.meta.url);
+globalThis.fs = require("fs");
+globalThis.TextEncoder = require("util").TextEncoder;
+globalThis.TextDecoder = require("util").TextDecoder;
 
 globalThis.performance = {
-	now() {
-		const [sec, nsec] = process.hrtime()
-		return sec * 1000 + nsec / 1000000
-	}
-}
+  now() {
+    const [sec, nsec] = process.hrtime();
+    return sec * 1000 + nsec / 1000000;
+  },
+};
 
 if (!globalThis.crypto) {
-	const crypto = require('crypto')
-	globalThis.crypto = {
-		getRandomValues(b) {
-			crypto.randomFillSync(b)
-		}
-	}
+  const crypto = require("crypto");
+  globalThis.crypto = {
+    getRandomValues(b) {
+      crypto.randomFillSync(b);
+    },
+  };
 }
